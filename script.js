9282c46d6b5c98bd3b55d283a1a67532d5c1a7f4
IsakOlafs <131982586+IsakOlafs@users.noreply.github.com>
2023-04-29 19:03:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -50,24 +50,3 @@ images.forEach(image => {
 fullScreen.addEventListener('click', () => {
   fullScreen.style.display = 'none';
 });
-
-  });
-});
-
-
-// Add click event listener to each image
-images.forEach(image => {
-  image.addEventListener('click', () => {
-    // Show the full-screen overlay
-    fullScreen.style.display = 'flex';
-
-    // Set the source of the full-screen image to the clicked image
-    fullScreenImage.src = image.src;
-  });
-});
-
-// Add click event listener to the full-screen overlay to hide it
-fullScreen.addEventListener('click', () => {
-  fullScreen.style.display = 'none';
-});
-
