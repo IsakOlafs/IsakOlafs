487aaf4d272c393df196e11e3474c7cdd1cb727e
IsakOlafs <131982586+IsakOlafs@users.noreply.github.com>
2023-04-29 18:48:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll('.gallery .image');
+const images = document.querySelectorAll('.gallery img');
 const fullScreen = document.getElementById('fullScreen');
 const fullScreenImage = document.getElementById('fullScreenImage');
 const galleryLinks = document.querySelectorAll('.dropdown-content a');
@@ -34,5 +34,22 @@ galleryLinks.forEach(link => {
       }
     });
   });
+});
+
+
+// Add click event listener to each image
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    // Show the full-screen overlay
+    fullScreen.style.display = 'flex';
+
+    // Set the source of the full-screen image to the clicked image
+    fullScreenImage.src = image.src;
+  });
+});
+
+// Add click event listener to the full-screen overlay to hide it
+fullScreen.addEventListener('click', () => {
+  fullScreen.style.display = 'none';
 });
 
